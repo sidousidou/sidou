91c7295460a675fcf4cd593bf600ba1eac6b3a11
sidousidou <45542282+sidousidou@users.noreply.github.com>
2021-07-29 23:03:56 +0100

    Update RDPwin11.sh

--- RDPwin11.sh
+++ RDPwin11.sh
@@ -4,7 +4,6 @@
 yum install unzip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 clear
-echo "Katacoda Centos Windows 11 by fb.com/elninosdo"
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
@@ -21,11 +20,10 @@ custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 clear
-echo "Katacoda Centos Windows 11 by fb.com/elninosdo"
 echo Your RDP IP Address is: 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
 echo Password: Thuonghai001
 echo Script by fb.com/elninosdo
-echo Wait 2-4m VM boot up before connect. 
-echo Do not close Katacoda tab. VM expired in 1 hour.
+echo Wait 2 to 4m VM boot up before connect. 
+echo Do not close Katacoda tab in the Browser. VM expired in 1 hour.
